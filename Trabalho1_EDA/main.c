7907ea9dd9e5c2b54380ffbd28232a2572709795
Sidney Fernando <88115743+nando3d3@users.noreply.github.com>
2022-08-18 10:11:18 -0300

    Update main.c

--- Trabalho1_EDA/main.c
+++ Trabalho1_EDA/main.c
@@ -1,109 +1,222 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// C program for implementation of Bubble sort
 
-typedef struct {
-    char nome[2];
-    int atraso;
-} AIRLINE;
+//Função para realizar ação com arquivo
+//Passa nome do arquivo e oq deseja fazer
+FILE *carregaArquivo(char *file_name, char *a){
 
-//Bubble Sort
+    FILE *arquivo;
 
-void bubbleSort(int vet[], int tam){
-  int i,j, temp;
-  for(i=tam-1; i>0; i--){
-    for(j=0; j < i; j++) //Faz trocas até posição i
-      if( vet[j] > vet[j+1] ){
-        temp = vet[j];
-        vet[j] = vet[j+1];
-        vet[j+1] = temp;
-      }
-  }
+    arquivo = fopen(file_name, a);
+
+    if(arquivo != NULL){
+        if(a=="r"){
+            printf("Arquivo lido com sucesso!\n");
+        } else if(a=="w"){
+            printf("Arquivo criado com sucesso!\n");
+        } else{
+            printf("Arquivo localizado com sucesso!\n");
+        }
+        
+        return arquivo;
+    }else{
+        printf("Erro ao carregar o arquivo!\n");
+    }
+
+    return 0;
 }
+
+
+//Função para comparar duas strings
+int comparaString(char *a, char *b)
+{
+    int i;
+    int resp;
+
+    if (strlen(a) == strlen(b)) {
+        for (i = 0; i < strlen(a); i++) {
+            if (a[i] == b [i]) {
+                resp = 1;
+            } else {
+                return 0;
+            }
+        }
+    } else {
+        resp = 0;
+    }
+
+    return resp;
+}
+
+//Função para substituir Strings;
+setString(char *a, char *b){
+    int i,j;
+
+    for (i=0; i<strlen(a); i++){
+        b[i] = a[i];
+    }
+    b[i] = '\0';
+}
+
+
+typedef struct
+{
+    char name[2];
+    int qteVoos;
+    int atraso;
+    int qteAtraso;
+    float media;
+} Airline;
+
+//Função de ordenação BubbleSort
+//ordena as médias e os nomes e quantidade de voos em função da media;
+void BubbleSort(int n,Airline *x){
+		int i,j;
+		for (i=n-1; i>=1;i--){
+			for(j=0;j<i;j++){
+				if(x[j].media>x[j+1].media){
+					/*TROCA*/
+					char temp[2];
+                    setString(x[j].name, temp);
+                    setString(x[j+1].name, x[j].name);
+                    setString(temp, x[j+1].name);
+					
+					float aux1;
+					aux1 = x[j].media;
+					x[j].media = x[j+1].media;
+					x[j+1].media = aux1;
+					
+					int aux2;
+					aux2 = x[j].qteVoos;
+					x[j].qteVoos = x[j+1].qteVoos;
+					x[j+1].qteVoos = aux2;
+
+                    int aux3;
+					aux3 = x[j].qteAtraso;
+					x[j].qteAtraso = x[j+1].qteAtraso;
+					x[j+1].qteAtraso = aux3;
+				}
+			}
+		}
+        printf("Ordenado!\n");
+}
+
 
 int main()
-{    
-    FILE *arquivo; // Ponteiro do arquivo
-    AIRLINE a;
-    char aleatorio[50];
-    char *pt;
-    int i=1;
+{
+    FILE *VoosDelay, *mediaVoo;
+    VoosDelay = carregaArquivo("VoosDelay.csv", "r");
+    mediaVoo = carregaArquivo("mediaVoo.csv", "w");
+    fputs ("Airline,AverageDelay,amountDelay,amountFlight\n", mediaVoo);   //Coloca cabeçalho no arquivo gerado
 
+    //QUANTIDADE DE LINHAS UTILIZADA:
+    int dados = 539383;
+    
+    Airline* airline = malloc(dados*sizeof(Airline));    //Cria uma struct de 'dados' elementos
+    int cont = 0;
+    int ler, i,j;
+    char charMedia[6], charVoo[7], charAtraso[7];
 
-    if(arquivo = fopen("TestData.csv", "r")) { // Abre o arquivo para leitura
-        fseek(arquivo, 0, SEEK_SET); // Posiciona o ponteiro no inicio do arquivo
+    clock_t tempo;
 
-        while(!feof(arquivo)) { // A funcao feof retorna 0 quando o ponteiro atinge o final do arquivo
-
-            fscanf(arquivo, "%[^;]s", a.nome);
-            fseek(arquivo, +1, SEEK_CUR);
-            fscanf(arquivo, "%d[^\n]", &a.atraso);
-
-            printf("%s|%d", a.nome, a.atraso);
-
-
-        }
-        fclose(arquivo);
-    } else {
-        printf("\nErro ao abrir arquivo\n");
-        getch();
+    //Para que a quantidade voos e atraso comece em 0;
+    for (i=0; i<dados; i++){
+        airline[i].qteVoos = 0;
+        airline[i].qteAtraso = 0;
     }
+
+    do{
+        //Ler o nome da empresa (com 2 char) e o atraso
+        ler = fscanf(VoosDelay, "%2[^,],%d\n", airline[cont].name, 
+                        &airline[cont].atraso);
+        
+        if (ler == 2){
+            cont++;
+            for (i=0; i<cont; i++){
+                if (comparaString(airline[cont-1].name,airline[i].name)){
+                    airline[cont-1].qteVoos++;  //Conta quantas vezes o nome da empresa se repetiu, logo quantos voos têm;
+
+                    if(airline[i].atraso){
+                        airline[cont-1].qteAtraso++;    //Se houve atraso (1), a quantide de atraso da determinada empresa é incremetada
+                    }
+                }
+            }
+            if(cont==dados/2){
+                printf("Metade lido. Linha: %d\a\n\n", cont);
+            }
+            
+        }
+        //Caso alguma linha esteja em formato diferente de "XX,1"
+        if (ler != 2 && !feof(VoosDelay) && cont!=0){
+            printf("Formato incorreto na linha %d\n", cont);
+        }
+    
+    } while (!feof(VoosDelay));
+ 
+    printf("Linhas: %d\n", cont);
+
+    //Associa o valor final de quantidade de voos e atrasos para cada empresa, independente de quando ela aparece
+    for (i=cont;i >= 0; i--){
+        for (j = 0; j < cont; j++){
+            if(comparaString(airline[i].name,airline[j].name)){
+                airline[j].qteVoos = airline[i].qteVoos;
+                airline[j].qteAtraso = airline[i].qteAtraso;
+            }
+        }
+    }
+
+    //Salva as médias
+    for (i=0;i < cont; i++){
+        airline[i].media = (float)airline[i].qteAtraso/airline[i].qteVoos;
+    }
+
+    //Ordena as médias
+    BubbleSort(dados, airline);
+
+    //Elimina as repetições e salva no arquivo
+    int k =0;
+    for (i=0; i<cont; i++){
+
+        for (j=0;j<k;j++){
+            if (comparaString(airline[i].name,airline[j].name)){
+                break;
+                }
+        }
+
+        if (j==k){
+            setString(airline[i].name, airline[k].name);
+            airline[k].media = airline[i].media;
+            airline[k].qteVoos = airline[i].qteVoos;
+            airline[k].qteAtraso = airline[i].qteAtraso;
+
+            sprintf(charMedia, "%.3f", airline[k].media);
+            sprintf(charVoo, "%d", airline[k].qteVoos);
+            sprintf(charAtraso, "%d", airline[k].qteAtraso);
+
+            fputs(airline[k].name, mediaVoo);
+            fputs(",", mediaVoo);
+            fputs(charMedia, mediaVoo);
+            fputs(",", mediaVoo);
+            fputs(charAtraso, mediaVoo);
+            fputs(",", mediaVoo);
+            fputs(charVoo, mediaVoo);
+            fputs("\n", mediaVoo);
+            k++;
+        }
+
+        if(i==cont-1){
+            printf("Salvo em mediaVoo!\n");
+        }
+
+    }
+
+    tempo = clock();
+    printf("Tempo de execucao: %f segundos", (float)tempo/CLOCKS_PER_SEC);
+    fclose(mediaVoo);
+    fclose(VoosDelay);
+    free(airline);
+
     return 0;
-
-
 }
-
-
-// switch (a.nome)
-// {
-// case "AS":
-//     /* code */
-//     break;
-// case "AA":
-//     /* code */
-//     break;
-// case "AC":
-//     /* code */
-//     break;
-// case "AM":
-//     /* code */
-//     break;
-// case "CO":
-//     /* code */
-//     break;
-// case "DL":
-//     /* code */
-//     break;
-// case "FX":
-//     /* code */
-//     break;
-// case "HA":
-//     /* code */
-//     break;
-// case "NW":
-//     /* code */
-//     break;
-// case "PO":
-//     /* code */
-//     break;
-// case "SW":
-//     /* code */
-//     break;
-// case "UA":
-//     /* code */
-//     break;
-// case "5X":
-//     /* code */
-//     break;
-// case "VS"::
-//     /* code */
-//     break;
-// case "VB"::
-//     /* code */
-//     break;
-// case "WS"::
-//     /* code */
-//     break;
-// }
-
